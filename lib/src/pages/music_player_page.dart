b8ce30ff534c606701d86afd2551323ee2da95b4
= <=>
2022-05-23 10:31:36 -0400

    listado de letras de la canción  con ListWheenScrollView

--- lib/src/pages/music_player_page.dart
+++ lib/src/pages/music_player_page.dart
@@ -1,3 +1,4 @@
+import 'package:app_musicplayer/src/helpers/helpers.dart';
 import 'package:app_musicplayer/src/widgets/custom_appBar.dart';
 import 'package:flutter/material.dart';
 
@@ -9,8 +10,63 @@ class MusicPlayerPage extends StatelessWidget {
       children: [
         CustomAppBar(),
         ImageDiscoDuracion(),
+        TituloPlay(),
+        Expanded(child: Lyrics())
       ],
     ));
+  }
+}
+
+class Lyrics extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final lyrics = getLyrics();
+    return Container(
+      child: ListWheelScrollView(
+        physics: const BouncingScrollPhysics(),
+        itemExtent: 42,
+        diameterRatio: 1.5,
+        children: lyrics
+            .map((linea) => Text(
+                  linea,
+                  style: TextStyle(
+                      fontSize: 20, color: Colors.white.withOpacity(0.8)),
+                ))
+            .toList(),
+      ),
+    );
+  }
+}
+
+class TituloPlay extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 40),
+      margin: const EdgeInsets.only(top: 60),
+      child: Row(
+        children: [
+          Column(
+            children: [
+              Text('Far Away',
+                  style: TextStyle(
+                      fontSize: 30, color: Colors.white.withOpacity(0.8))),
+              Text('-Breaking Benjamin-',
+                  style: TextStyle(
+                      fontSize: 15, color: Colors.white.withOpacity(0.5))),
+            ],
+          ),
+          const Spacer(),
+          FloatingActionButton(
+            elevation: 0,
+            highlightElevation: 0,
+            onPressed: () {},
+            backgroundColor: const Color(0xfff8cb51),
+            child: const Icon(Icons.play_arrow),
+          )
+        ],
+      ),
+    );
   }
 }
 
@@ -18,11 +74,49 @@ class ImageDiscoDuracion extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 30),
+      margin: const EdgeInsets.only(top: 70),
       child: Row(
         children: [
-          ImageDisco()
+          ImageDisco(),
+          const SizedBox(width: 20),
+          BarraProgreso(),
+          const SizedBox(width: 20),
+        ],
+      ),
+    );
+  }
+}
 
-          //TODO:Barra progreso
+class BarraProgreso extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final estilo = TextStyle(color: Colors.white.withOpacity(0.4));
+
+    return Container(
+      child: Column(
+        children: [
+          Text('00:00', style: estilo),
+          const SizedBox(height: 10),
+          Stack(
+            children: [
+              Container(
+                width: 3,
+                height: 230,
+                color: Colors.white.withOpacity(0.3),
+              ),
+              Positioned(
+                bottom: 0,
+                child: Container(
+                  width: 3,
+                  height: 150,
+                  color: Colors.white.withOpacity(0.8),
+                ),
+              )
+            ],
+          ),
+          const SizedBox(height: 10),
+          Text('00:00', style: estilo),
         ],
       ),
     );
